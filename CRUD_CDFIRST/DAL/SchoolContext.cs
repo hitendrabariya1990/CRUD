19a0280bc6dfc0c0c0846e26f85448b4bc0ff378
unknown <Admin@Payalt.promact.com>
2015-08-08 10:23:59 +0530

    Code First Approach

---
+++ CRUD_CDFIRST/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using CRUD_CDFIRST.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace CRUD_CDFIRST.DAL
+{
+    public class SchoolContext:DbContext 
+    {
+        public SchoolContext():base("SchoolContext")
+        {
+
+        }
+
+        public DbSet<Student> Student { get; set; }
+        public DbSet<Enrollment> Enrollment { get; set; }
+        public DbSet<Course> Course { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
